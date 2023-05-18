2988a77622968300aaa0b98772f54776593486c9
Valentin Li <li.valentin2@gmail.com>
2023-05-18 15:59:44 +0200

    Mise à jours mineur La route add new kilo ne marche pas ?

--- screens/BuyScreen.js
+++ screens/BuyScreen.js
@@ -1,11 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
-export default function BuyScreen() {
+export default function BuyScreen({ route }) {
+  console.log('buyScreen route:', route);
+
+  const { flightId } = route.params;
+
+  if (!flightId) {
+    // Traitez le cas où la propriété 'flightId' est manquante ou indéfinie
+    return (
+      <View style={styles.container}>
+        <Text>Erreur: flightId est manquant</Text>
+      </View>
+    );
+  }
+
+  // Utilisez l'identifiant du vol comme bon vous semble dans votre composant BuyScreen
 
   return (
     <View style={styles.container}>
-        <Text>Ici on sort sa carte</Text>
+      <Text>Flight ID: {flightId}</Text>
+      <Text>Ici on sort sa carte</Text>
     </View>
   );
 }
