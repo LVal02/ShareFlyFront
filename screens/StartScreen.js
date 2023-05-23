10ef4eaf27f22b6ebdd599778c7d3aeebf603e0d
Leo <leo.nhari18@gmail.com>
2023-05-23 17:59:46 +0200

    small fix

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -44,7 +44,6 @@ export default function StartScreen() {
   const [errorMessage, setErrorMessage] = useState("");
   const [isTextVisible, setTextVisibility] = useState(1);
 
-
   const handleTogglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
@@ -57,11 +56,11 @@ export default function StartScreen() {
 
   const disapear = () => {
     setTextVisibility(isTextVisible * -1);
-    console.log(isTextVisible) 
-    console.log(isTextVisible == true) 
-  }
+    console.log(isTextVisible);
+    console.log(isTextVisible == true);
+  };
   const handleSubmit = () => {
-    //le signUp classique 
+    //le signUp classique
     if (EMAIL_REGEX.test(email)) {
       fetch("https://share-fly-backend.vercel.app/users/signin", {
         method: "POST",
@@ -70,37 +69,38 @@ export default function StartScreen() {
       })
         .then((response) => response.json())
         .then((data) => {
-          console.log("dataFetchsignIn",data);
+          console.log("dataFetchsignIn", data);
           if (data.result) {
             dispatch(addToken(data.token));
             dispatch(updateUsername(data.username));
-            
+
             dispatch(updateFirstname(data.firstname)); // Mettre à jour le firstname dans le reducer
-            dispatch(updateLastname(data.lastname)); 
+            dispatch(updateLastname(data.lastname));
             //On veut récupérer l'objectId de vol
-            const objetRequest = { token: data.token, username: data.username }
+            const objetRequest = { token: data.token, username: data.username };
             console.log("ça passe ici objetRequest:", objetRequest);
             fetch("https://share-fly-backend.vercel.app/flights", {
               method: "POST",
               headers: { "Content-Type": "application/json" },
               body: JSON.stringify(objetRequest),
             })
-            .then((response) => response.json())
-            .then((dataFlight) => {
-              console.log("ça passe aussi ici dataFlight", dataFlight);
-              if (dataFlight.result) {
-                console.log(dataFlight);
+              .then((response) => response.json())
+              .then((dataFlight) => {
+                console.log("ça passe aussi ici dataFlight", dataFlight);
+                if (dataFlight.result) {
+                  console.log(dataFlight);
 
-                const {_id, flyNumber, date} = dataFlight.data[0] 
-                console.log("_id, flyNumber, date",_id, flyNumber, date); 
-                dispatch(updateFlightObjectId(_id))
-                dispatch(updateFlyNumber(flyNumber))
-                dispatch(updateDate(date))
-                // A Trouvé un vol donc il navigue avec un n° de vol et son id
-               navigation.navigate("TabNavigator", { screen: "Home" });
-              }else { navigation.navigate("TabNavigator", { screen: "Home" }) // N'a pas encore enregistrer de vol
-            }
-            })
+                  const { _id, flyNumber, date } = dataFlight.data[0];
+                  console.log("_id, flyNumber, date", _id, flyNumber, date);
+                  dispatch(updateFlightObjectId(_id));
+                  dispatch(updateFlyNumber(flyNumber));
+                  dispatch(updateDate(date));
+                  // A Trouvé un vol donc il navigue avec un n° de vol et son id
+                  navigation.navigate("TabNavigator", { screen: "Home" });
+                } else {
+                  navigation.navigate("TabNavigator", { screen: "Home" }); // N'a pas encore enregistrer de vol
+                }
+              });
           } else {
             setErrorMessage("Invalid email or password");
           }
@@ -114,7 +114,7 @@ export default function StartScreen() {
   const autoLogin = () => {
     dispatch(addToken("IjY0bA1PT-shvwqTZRKculkRbP3atUU-"));
     dispatch(updateUsername("Devxi"));
-    dispatch(updateDate("2023-05-01T00:00:00.000Z"));
+    dispatch(updateDate("2023-05-01T00:00:00.000"));
     dispatch(updateFlyNumber("OZ110"));
     dispatch(updateFlightObjectId("3lLVDtDGdyye2nTbYzz9ZcTME8gR4bgB"));
     navigation.navigate("TabNavigator", { screen: "Home" });
@@ -145,88 +145,93 @@ export default function StartScreen() {
         <Text style={styles.textButton}>AutoLogin2 noFetch Token+username</Text>
       </TouchableOpacity>
 
-  <View style={styles.titre}>
-    <FontAwesome name="plane" size={50} color="#112f5b" style={styles.logo} />
-    <Text style={styles.title}>Welcome to ShareFly</Text>
-  </View>
-      
-        {/* <KeyboardAvoidingView
+      <View style={styles.titre}>
+        <FontAwesome
+          name="plane"
+          size={50}
+          color="#112f5b"
+          style={styles.logo}
+        />
+        <Text style={styles.title}>Welcome to ShareFly</Text>
+      </View>
+
+      {/* <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
         
       > */}
-        <View style={styles.inputContainer}>
-          <TextInput
-            placeholder="Email"
-            placeholderTextColor="#8f9094"
-            autoCapitalize="none"
-            keyboardType="email-address"
-            textContentType="emailAddress"
-            autoCompleteType="email"
-            onChangeText={(value) => setEmail(value)}
-            // onTouchStart={() => disapear()}
-            // onBlur={() => disapear()}
-            
-            value={email}
-            style={styles.input}
-          />
-          <TextInput
-            placeholder="Password"
-            placeholderTextColor="#8f9094"
-            autoCapitalize="none"
-            secureTextEntry={!showPassword}
-            onChangeText={(value) => setPassword(value)}
-            value={password}
-            style={styles.input}
-            // onTouchStart={() => disapear()}
-            // onBlur={() => disapear()}
-          />
-          <TouchableOpacity
-            onPress={handleTogglePasswordVisibility}
-            style={styles.iconButton}
-          >
-            <FontAwesome
-              name={showPassword ? "eye-slash" : "eye"}
-              size={24}
-              color="black"
-            />
-          </TouchableOpacity>
-          {errorMessage && (
-            <Text style={styles.error}>Invalid email address</Text>
-          )}
-          <TouchableOpacity
-            onPress={() => handleSubmit()}
-            style={styles.logInbutton}
-            activeOpacity={0.8}
-          >
-            <Text style={styles.logIntextButton}>Log In</Text>
-          </TouchableOpacity>
-        </View>
-        {/* </KeyboardAvoidingView> */}
-        <TouchableOpacity
-          onPress={() => navigation.navigate("SignUp")}
-          style={styles.button}
-          activeOpacity={0.8}
-        >
-          <Text style={styles.textButton}>Sign Up</Text>
-        </TouchableOpacity>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Email"
+          placeholderTextColor="#8f9094"
+          autoCapitalize="none"
+          keyboardType="email-address"
+          textContentType="emailAddress"
+          autoCompleteType="email"
+          onChangeText={(value) => setEmail(value)}
+          // onTouchStart={() => disapear()}
+          // onBlur={() => disapear()}
 
+          value={email}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Password"
+          placeholderTextColor="#8f9094"
+          autoCapitalize="none"
+          secureTextEntry={!showPassword}
+          onChangeText={(value) => setPassword(value)}
+          value={password}
+          style={styles.input}
+          // onTouchStart={() => disapear()}
+          // onBlur={() => disapear()}
+        />
         <TouchableOpacity
-          onPress={() => navigation.navigate("Snap")}
-          style={styles.button}
+          onPress={handleTogglePasswordVisibility}
+          style={styles.iconButton}
+        >
+          <FontAwesome
+            name={showPassword ? "eye-slash" : "eye"}
+            size={24}
+            color="black"
+          />
+        </TouchableOpacity>
+        {errorMessage && (
+          <Text style={styles.error}>Invalid email address</Text>
+        )}
+        <TouchableOpacity
+          onPress={() => handleSubmit()}
+          style={styles.logInbutton}
           activeOpacity={0.8}
         >
-          <Text style={styles.textButton}>Go To Snap</Text>
+          <Text style={styles.logIntextButton}>Log In</Text>
         </TouchableOpacity>
+      </View>
+      {/* </KeyboardAvoidingView> */}
+      <TouchableOpacity
+        onPress={() => navigation.navigate("SignUp")}
+        style={styles.button}
+        activeOpacity={0.8}
+      >
+        <Text style={styles.textButton}>Sign Up</Text>
+      </TouchableOpacity>
 
-        <TouchableOpacity
-          onPress={() => navigation.navigate("FlightBoard")}
-          style={styles.button}
-          activeOpacity={0.8}
-        >
-          <Text style={styles.textButton}>
-            Annonce Ici, A déplacer quand FlightSub sera finit
-          </Text>
-        </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Snap")}
+        style={styles.button}
+        activeOpacity={0.8}
+      >
+        <Text style={styles.textButton}>Go To Snap</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate("FlightBoard")}
+        style={styles.button}
+        activeOpacity={0.8}
+      >
+        <Text style={styles.textButton}>
+          Annonce Ici, A déplacer quand FlightSub sera finit
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -234,33 +239,32 @@ export default function StartScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
     alignItems: "center",
     justifyContent: "center",
     marginBottom: 60,
   },
   logo: {
-  // backgroundColor: 'red',
-  paddingRight: 2,
+    // backgroundColor: 'red',
+    paddingRight: 2,
   },
   title: {
     fontSize: 50,
     fontWeight: "600",
     color: "#000000",
     alignItems: "center",
-    textAlign: "center", 
-  marginBottom: 40,   
+    textAlign: "center",
+    marginBottom: 40,
   },
   titre: {
     flexDirection: "row",
-    marginRight: 30
+    marginRight: 30,
   },
   inputContainer: {
     padding: 30,
     borderRadius: 10,
-    width: 350
-
+    width: 350,
   },
   input: {
     width: "100%",
@@ -285,7 +289,7 @@ const styles = StyleSheet.create({
     opacity: 0.9,
     borderRadius: 5,
     elevation: 5,
-    shadowColor: '#000000',
+    shadowColor: "#000000",
     shadowOpacity: 0.3,
     shadowRadius: 5,
     shadowOffset: { width: 0, height: 2 },
